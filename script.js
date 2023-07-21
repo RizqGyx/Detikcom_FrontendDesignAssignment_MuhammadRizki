64fcf68f5b92b77a356a617ba4cb08404089ade3
Rizki <106717697+RizqGyx@users.noreply.github.com>
2023-07-21 21:41:04 +0700

    Function Animation

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const box = (image, text) => {
-    const src = `./src/latest/${image}`;
-    const news = text;
+  const src = `./src/latest/${image}`;
+  const news = text;
 
-    
+
+};
+
+function brandAnimation() {
+    const brands = document.querySelector("ul.photos"),
+                   total_brand = brands.children.length;
+
+    for (let i = 0; i < total_brand; i++) {
+        brands.appendChild(brands.children[i].cloneNode(true));
+    }
 }
