32aad0e32e7ae758bfa5da6b400dfa86764f3a2e
Rizki <106717697+RizqGyx@users.noreply.github.com>
2023-07-23 11:30:22 +0700

    Non Functional Register Page

--- src/components/navbarHead.js
+++ src/components/navbarHead.js
@@ -1,6 +1,6 @@
 export const Header = () => {
     return `<div class="container">
-            <a href="#">
+            <a href="welcomePage.html">
                 <img src="./src/background/Navbar Brand.png" alt="Brand">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,13 +9,13 @@ export const Header = () => {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto navbar_content_ul">
                     <li class="nav-item">
-                        <a class="nav-link" href="#about">About</a>
+                        <a class="nav-link" href="welcomePage.html#about">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#mechanism">Mechanism</a>
+                        <a class="nav-link" href="welcomePage.html#mechanism">Mechanism</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#latest">Latest Update</a>
+                        <a class="nav-link" href="welcomePage.html#latest">Latest Update</a>
                     </li>
                     <li class="nav-item register">
                         <a class="nav-link" href="register.html">Register</a>
